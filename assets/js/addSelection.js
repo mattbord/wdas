e0b9f828bf19c909dfdccebdf122ab20d1e1e478
mattbord <71492701+mattbord@users.noreply.github.com>
2023-04-01 20:54:29 +1100

    login page and image folders restructure

--- assets/js/addSelection.js
+++ assets/js/addSelection.js
@@ -1,12 +1,18 @@
 
 function addSelection(item){
     const number = document.querySelectorAll('#main .options-card').length;
-    if (number>=10){
-        alert("Max items exceeded")
+    if (number>=10 || !!document.getElementById(item)){
+        if(number>=10){
+            alert("Maximum of 10 ingredients allowed")
+        } else {
+            alert("Cannot select same ingredient twice")
+        }
+        
     } else {
         const card = document.createElement("div");
         card.className = "options-card"
         card.textContent = item
+        card.setAttribute("id", item)
     
         var container = document.getElementById('selection-container')
         container.appendChild(card);
